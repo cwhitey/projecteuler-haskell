19f5c59dd2a9b1b640342dcceb7e5b5016ef75cf
Callum White <callum.white@rea-group.com>
2016-12-30 15:38:48 +1100

    updates to ex 1

--- 1.hs
+++ 1.hs
@@ -4,5 +4,11 @@ modThree x = x `mod` 3 == 0
 modFive :: Int -> Bool
 modFive x = x `mod` 5 == 0
 
+getNumbers :: Int -> [Int]
+getNumbers maxInt = [ x | x <- [0..maxInt], modThree x || modFive x]
+
+sumNumbers :: Int
+sumNumbers = foldr (+) 0 (getNumbers 1000)
+
 main :: IO()
-main = print [ x | x <- [0..1000], modThree x || modFive x]
+main = print sumNumbers
